053fe3e0834eadeb784d80489d9c900ff4798dd0
Austin Dunn <austin.dunn707@gmail.com>
2022-11-13 22:08:25 -0800

    clean up

--- ipinfo/ipLookup.py
+++ ipinfo/ipLookup.py
@@ -15,14 +15,10 @@ def user(name):
 @app.route('/ip/<ip>')
 def ip(ip):
 	return ipinfo(ip)
-	n = 0
-	while n <= len(ipinfo(ip)):
-		return ipinfo(ip)[n]
-		n += 1
 
 def ipinfo(ip):
 	r = re.get(f'http://ipinfo.io/{ip}?token=d3de8a5e2d4c60')
 	return [x.strip(' ') for x in r.text.split('\n')[1:len(ip)-1]]
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run()
