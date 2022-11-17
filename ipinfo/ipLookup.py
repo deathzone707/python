874ea11a90aef412dcfeabdf8cdd3b3994d3c13f
Austin Dunn <austin.dunn707@gmail.com>
2022-11-17 10:41:20 -0800

    using html

--- ipinfo/ipLookup.py
+++ ipinfo/ipLookup.py
@@ -1,25 +1,23 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 from os import environ
 import requests as re
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return "Hello! This is the main page. <h1>WELCOME HUMAN</h1>"
-
-@app.route("/<name>")
-def user(name):
-	return f"This is the name page. {name}"
+	return render_template('index.html')
 
 @app.route('/ip/<ip>')
 def ip(ip):
-	return ipinfo(ip)
+	return render_template('ipinfo.html', ipinformation=ipinfo(ip))
 
 def ipinfo(ip):
 	token = environ['IPINFOTOKEN']
 	r = re.get(f'http://ipinfo.io/{ip}?token={token}')
-	return [x for x in r.text.split('\n')[1:len(ip)-1]]
+	r = json.loads(r.text)
+	return r
 
 if __name__ == "__main__":
 	app.run()
